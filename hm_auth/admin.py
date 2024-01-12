c114b53bec3c467326166375d18bcd4b54dd9def
David Ekong <66387173+davidekong@users.noreply.github.com>
2024-01-12 15:08:46 +0000

    Pending changes exported from your codespace

--- hm_auth/admin.py
+++ hm_auth/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Patient, HospitalPersonnel, Hospital, MedicalRecord
 # Register your models here.
+admin.site.register(Patient)
+admin.site.register(HospitalPersonnel)
+admin.site.register(Hospital)
+admin.site.register(MedicalRecord)
--- hm_auth/urls.py
+++ hm_auth/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('login/', views.logIn, name='login'),
     path('home/', views.home, name='home'),
-    path('add-an-account/', views.add_account, name='add_account'),
-    path('add-a-patient/', views.add_patient, name='add_patient'),
-    path('add-a-personnel/', views.add_personnel, name='add_personnel'),
+    path('pick-an-account/', views.choose_add_account, name='choose_add_account'),
+    path('add-patient/', views.add_patient, name='add_patient'),
+    path('join-hospital/', views.join_hospital, name='join_hospital'),
+    path('add-hospital/', views.add_hospital, name='add_hospital'),
+    path('logout/', views.logout_user, name='logout'),
+    path('table/', views.table, name='table'),
 ]
